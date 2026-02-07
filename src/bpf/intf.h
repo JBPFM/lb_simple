61395825373e9a831e8a6e8afaf7faa58fbef391
JerryMouseZ <1711333@mail.nankai.edu.cn>
2026-02-08 00:02:41 +0800

    backup

--- src/bpf/intf.h
+++ src/bpf/intf.h
@@ -6,6 +6,21 @@
 #define __kptr
 #endif
 
+#define VIP_DSQ_ID 1
+
+enum yield_reason {
+    YIELD_NONE            = 0,
+    YIELD_LOCK_CONTENTION = 1,
+    YIELD_LOCK_HANDOFF    = 2,
+};
+
+/* 存储在用户空间 thread-local 中，BPF 通过 bpf_probe_read_user 读取 */
+struct task_yield_info {
+    unsigned int reason;           /* enum yield_reason */
+    unsigned int gen;              /* 代数计数器，每次 yield 递增 */
+    unsigned long long lock_addr;
+};
+
 struct held_lock_info {
     unsigned long long lock_addr;
     unsigned long long hold_start_ns;
